6a784450a5518411c7c62eeb725d0cc56f78fed2
Philipp Wagner <bytefish@gmx.de>
2016-05-21 13:07:10 +0200

    Release
    Correct Package Names. Adding Deployment Scripts.

---
+++ ElasticUtils/src/test/java/de/bytefish/elasticutils/mapping/LocalWeatherDataMapper.java
@@ -0,0 +1,48 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package de.bytefish.elasticutils.mapping;
+
+import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.mapper.core.DateFieldMapper;
+import org.elasticsearch.index.mapper.core.FloatFieldMapper;
+import org.elasticsearch.index.mapper.core.StringFieldMapper;
+import org.elasticsearch.index.mapper.geo.GeoPointFieldMapper;
+import org.elasticsearch.index.mapper.object.ObjectMapper;
+import org.elasticsearch.index.mapper.object.RootObjectMapper;
+
+public class LocalWeatherDataMapper extends BaseElasticSearchMapping {
+
+    private static final String INDEX_TYPE = "document";
+
+    public LocalWeatherDataMapper() {
+        super(INDEX_TYPE, "1.0.0");
+    }
+
+    @Override
+    protected void configure(RootObjectMapper.Builder builder) {
+        builder
+                .add(new DateFieldMapper.Builder("dateTime"))
+                .add(new FloatFieldMapper.Builder("temperature"))
+                .add(new FloatFieldMapper.Builder("windSpeed"))
+                .add(new FloatFieldMapper.Builder("stationPressure"))
+                .add(new StringFieldMapper.Builder("skyCondition"))
+                .add(new ObjectMapper.Builder("station")
+                        .add(new StringFieldMapper.Builder("wban"))
+                        .add(new StringFieldMapper.Builder("name"))
+                        .add(new StringFieldMapper.Builder("state"))
+                        .add(new StringFieldMapper.Builder("location"))
+                        .add(new GeoPointFieldMapper.Builder("coordinates")
+                                .enableLatLon(true)
+                                .enableGeoHash(false))
+                        .nested(ObjectMapper.Nested.newNested(true, false)));
+    }
+
+    @Override
+    protected void configure(Settings.Builder builder) {
+        builder
+                .put(IndexMetaData.SETTING_VERSION_CREATED, 1)
+                .put(IndexMetaData.SETTING_CREATION_DATE, System.currentTimeMillis());
+    }
+}
---
+++ ElasticUtils/src/test/java/de/bytefish/elasticutils/utils/ElasticSearchUtilsTest.java
@@ -0,0 +1,51 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package de.bytefish.elasticutils.utils;
+
+import de.bytefish.elasticutils.exceptions.CreateIndexFailedException;
+import de.bytefish.elasticutils.exceptions.IndicesExistsFailedException;
+import de.bytefish.elasticutils.exceptions.PutMappingFailedException;
+import org.elasticsearch.client.Client;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class ElasticSearchUtilsTest {
+
+    @Mock
+    private Client client;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
+
+    @Test
+    public void indices_exist_throws_correct_exception_when_underlying_client_throws() throws Exception {
+
+        when(client.admin()).thenThrow(new RuntimeException());
+
+        TestUtils.assertThrows(() -> ElasticSearchUtils.indexExist(client, ""), IndicesExistsFailedException.class);
+    }
+
+    @Test
+    public void create_index_throws_correct_exception_when_underlying_client_throws() throws Exception {
+
+        when(client.admin()).thenThrow(new RuntimeException());
+
+        TestUtils.assertThrows(() -> ElasticSearchUtils.createIndex(client, ""), CreateIndexFailedException.class);
+    }
+
+
+    @Test
+    public void put_mapping_throws_correct_exception_when_underlying_client_throws() throws Exception {
+
+        when(client.admin()).thenThrow(new RuntimeException());
+
+        TestUtils.assertThrows(() -> ElasticSearchUtils.putMapping(client, "", null), PutMappingFailedException.class);
+    }
+}
